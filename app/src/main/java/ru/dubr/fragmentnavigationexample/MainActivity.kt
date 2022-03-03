94bff63334f7cdbd2bebafe61e7f4f076df6b75c
Dubravin Evgeny <dubrdubr23@gmail.com>
2022-03-03 03:21:11 +0300

    create MainActivity

--- app/src/main/java/ru/dubr/fragmentnavigationexample/MainActivity.kt
+++ app/src/main/java/ru/dubr/fragmentnavigationexample/MainActivity.kt
@@ -1,13 +1,21 @@
 package ru.dubr.fragmentnavigationexample
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcelable
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentResultListener
 import androidx.lifecycle.LifecycleOwner
 import ru.dubr.fragmentnavigationexample.R
-import ru.dubr.fragmentnavigationexample.contracts.Navigator
-import ru.dubr.fragmentnavigationexample.contracts.ResultListener
+import ru.dubr.fragmentnavigationexample.contracts.*
 import ru.dubr.fragmentnavigationexample.databinding.ActivityMainBinding
 import ru.dubr.fragmentnavigationexample.fragments.MenuFragment
 import ru.dubr.fragmentnavigationexample.fragments.PlanetDetailsFragment
@@ -20,6 +28,17 @@ class MainActivity : AppCompatActivity(), Navigator {
     private val currentFragment: Fragment
         get() = supportFragmentManager.findFragmentById(R.id.fragmentContainer)!!
 
+    private val fragmentListener = object : FragmentManager.FragmentLifecycleCallbacks() {
+        override fun onFragmentViewCreated(
+            fm: FragmentManager,
+            f: Fragment,
+            v: View,
+            savedInstanceState: Bundle?
+        ) {
+            super.onFragmentViewCreated(fm, f, v, savedInstanceState)
+            updateUI()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +52,24 @@ class MainActivity : AppCompatActivity(), Navigator {
                 .commit()
         }
 
+        supportFragmentManager.registerFragmentLifecycleCallbacks(fragmentListener, false)
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        supportFragmentManager.unregisterFragmentLifecycleCallbacks(fragmentListener)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // we've called setSupportActionBar in onCreate,
+        // that's why we need to override this method too
+        updateUI()
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 
     override fun showPlanetDetailScreen(planet: Planet) {
@@ -49,7 +85,10 @@ class MainActivity : AppCompatActivity(), Navigator {
     }
 
     override fun <T : Parcelable> publishResult(result: T) {
-        TODO("Not yet implemented")
+        supportFragmentManager.setFragmentResult(
+            result.javaClass.name,
+            bundleOf(KEY_RESULT to result)
+        )
     }
 
     override fun <T : Parcelable> listenResult(
@@ -57,7 +96,9 @@ class MainActivity : AppCompatActivity(), Navigator {
         owner: LifecycleOwner,
         listener: ResultListener<T>
     ) {
-        TODO("Not yet implemented")
+        supportFragmentManager.setFragmentResultListener(clazz.name, owner) { _, bundle ->
+            listener.invoke(bundle.getParcelable(KEY_RESULT)!!)
+        }
     }
 
     private fun launchFragment(fragment: Fragment) {
@@ -66,5 +107,48 @@ class MainActivity : AppCompatActivity(), Navigator {
             .addToBackStack(null)
             .replace(R.id.fragmentContainer, fragment)
             .commit()
+    }
+
+    private fun updateUI() {
+        val fragment = currentFragment
+
+        if (fragment is HasCustomTitle) {
+            binding.toolbar.title = getString(fragment.getTitleRes())
+        } else {
+            binding.toolbar.title = getString(R.string.default_title)
+        }
+
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            supportActionBar?.setDisplayShowHomeEnabled(true)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        } else {
+            supportActionBar?.setDisplayShowHomeEnabled(false)
+            supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        }
+
+        if (fragment is HasCustomAction) {
+            createCustomToolbarAction(fragment.getCustomAction())
+        } else {
+            binding.toolbar.menu.clear()
+        }
+    }
+
+    private fun createCustomToolbarAction(action: CustomAction) {
+        binding.toolbar.menu.clear()
+
+        val iconDrawable = DrawableCompat.wrap(ContextCompat.getDrawable(this, action.iconRes)!!)
+        iconDrawable.setTint(Color.WHITE)
+
+        val menuItem = binding.toolbar.menu.add(action.textRes)
+        menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+        menuItem.icon = iconDrawable
+        menuItem.setOnMenuItemClickListener {
+            action.onCustomAction.run()
+            return@setOnMenuItemClickListener true
+        }
+    }
+
+    companion object {
+        private const val KEY_RESULT = "KEY_RESULT"
     }
 }
